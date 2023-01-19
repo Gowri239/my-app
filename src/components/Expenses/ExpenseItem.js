532728ce98152ff22699589738e407a1b963c5c2
Gowri239 <gowrimopuru14@gmail.com>
2023-01-19 21:14:11 +0530

    adding event listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,16 +3,27 @@ import ExpenseAmount from './ExpenseDetails'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
+
+    const deleteExpense = () => {
+        document.getElementById('delete').remove()
+
+    }
+
     
     return (
-        <div className="expense-item">
+        <div id="delete">
+            <div className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <h2>{props.location}</h2>
                 <ExpenseAmount amount={props.amount}/>
             </div>
+              <button onClick={deleteExpense}>Delete Expense</button>
+            </div>
+
         </div>
+        
     
     )
 
