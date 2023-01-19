927b34a4542538d9d8e1cb3877bf5e301fe700ee
Gowri239 <gowrimopuru14@gmail.com>
2023-01-19 15:34:56 +0530

    outputting dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,18 @@
+import './ExpenseItem.css'
+
 function ExpenseItem(){
+    const expenseDate = new Date(2023,0,19)
+    const expenseTitle = 'car Insurance'
+    const expenseAmount = 2500
+    const locationOfExpenditure = "tirupati"
     return (
-        <div>
-            <div>
-              <h2>Expense item</h2>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <h2>{locationOfExpenditure}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
-            <div>Food Rs.100</div>
-            <div>Petrol Rs.200</div>
-            <div>Movies Rs.300</div>
         </div>
     
     )
