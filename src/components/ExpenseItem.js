c521077a46aa9de9919aa1cbe0bf7b6b12681aed
Gowri239 <gowrimopuru14@gmail.com>
2023-01-19 15:04:59 +0530

    ExpenseItems hardcoded

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <div>
+              <h2>Expense item</h2>
+            </div>
+            <div>Food Rs.100</div>
+            <div>Petrol Rs.200</div>
+            <div>Movies Rs.300</div>
+        </div>
+    
+    )
+}
+
+export default ExpenseItem
