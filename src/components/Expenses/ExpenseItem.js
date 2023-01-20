dda42764193ceba73c333f4754e9b14228699af1
Gowri239 <gowrimopuru14@gmail.com>
2023-01-20 10:33:40 +0530

    using useState for updating dom

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,4 @@
+import React,{useState} from 'react';
 import ExpenseDate from './ExpenseDate'
 import ExpenseAmount from './ExpenseDetails'
 import './ExpenseItem.css'
@@ -6,7 +7,18 @@ const ExpenseItem = (props) => {
 
     const deleteExpense = () => {
         document.getElementById('delete').remove()
+    }
 
+    let [title,setTitle] = useState(props.title)
+    let [amount,setAmount] = useState(props.amount)
+
+    const changeTitle = () => {
+        setTitle('updated!')
+        console.log(title)
+    }
+
+    const changeAmount = () => {
+        setAmount(100)
     }
 
     
@@ -15,13 +27,14 @@ const ExpenseItem = (props) => {
             <div className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <h2>{props.location}</h2>
-                <ExpenseAmount amount={props.amount}/>
+                <ExpenseAmount amount={amount}/>
             </div>
               <button onClick={deleteExpense}>Delete Expense</button>
+              <button onClick={changeTitle}>Change Title</button>
+              <button onClick={changeAmount}>Change amount</button>
             </div>
-
         </div>
         
     
