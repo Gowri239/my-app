6b640d5a052c512c73ce9a0345177a5171371a96
Gowri239 <gowrimopuru14@gmail.com>
2023-01-23 15:23:09 +0530

    implementing filter feature

--- src/App.js
+++ src/App.js
@@ -5,36 +5,7 @@ import NewExpense from "./components/Expenses/NewExpenses/NewExpenses";
 
 
 const App = () => {
-  const expenses = [
-    // {
-    //   id: 'e1',
-    //   title: 'Toilet Paper',
-    //   amount: 94.12,
-    //   date: new Date(2020, 7, 14),
-    //   location: 'Tirupati'
-    // },
-    // { 
-    //   id: 'e2',
-    //   title: 'New TV',
-    //   amount: 799.49,
-    //   date: new Date(2021, 2, 12),
-    //   location: 'Venkatagiri'
-    //  },
-    // {
-    //   id: 'e3',
-    //   title: 'Car Insurance',
-    //   amount: 294.67,
-    //   date: new Date(2021, 2, 28),
-    //   location: 'Nellore'
-    // },
-    // {
-    //   id: 'e4',
-    //   title: 'New Desk (Wooden)',
-    //   amount: 450,
-    //   date: new Date(2021, 5, 12),
-    //   location: 'Gudur'
-    // },
-  ];
+  const expenses = [];
 
   const [addExpense,setAddExpense] = useState(expenses)
   const [filteredYear,setFilterdYear] = useState('2020')
@@ -51,60 +22,27 @@ const App = () => {
   const filterChangeHandler = selectedYear => {
     setFilterdYear(selectedYear)
   }
+
+  const filteredExpenses = addExpense.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
   return (
     <div>
       <div>
         <NewExpense onAddExpense={addExpenseHandler}/>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
       </div>
-      {/* <NewExpense onAddExpense={addExpenseHandler}/> */}
-      {/* <ExpensesFilter /> */}
+      
       {
-        addExpense.map(expense => {
+        filteredExpenses.map(expense => {
           return <ExpenseItem
             key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
           ></ExpenseItem>
-
-          
         })
       }
-
-      
-      
-        
-        
-      {/* <NewExpense /> */}
-      {/* <ExpenseItem
-        id={expenses[0].id}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-        location={expenses[0].location}
-      />
-      <ExpenseItem
-        id={expenses[1].id}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        location={expenses[1].location}
-      />
-      <ExpenseItem
-        id={expenses[2].id}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        location={expenses[2].location}
-      />
-      <ExpenseItem
-        id={expenses[3].id}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        location={expenses[3].location}
-      />  */}
     </div>
   );
 }
