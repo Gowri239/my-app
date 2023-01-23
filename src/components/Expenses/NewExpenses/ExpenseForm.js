751365ed522283726ff37db941cc92229f1df60a
Gowri239 <gowrimopuru14@gmail.com>
2023-01-23 10:31:19 +0530

    using multiple states in one component

--- src/components/Expenses/NewExpenses/ExpenseForm.js
+++ src/components/Expenses/NewExpenses/ExpenseForm.js
@@ -1,16 +1,24 @@
 import './ExpenseForm.css'
+import {useState} from 'react'
 
 const ExpenseForm = () => {
+
+    const [enteredTitle,setEnteredTitle] = useState('');
+
+    const [enteredAmount,setEnteredAmount] = useState('');
+
+    const [enteredDate,setEnteredDate] = useState('')
+
     const titleChangeHandler = (event) => {
-        console.log(event.target.value)
+        setEnteredTitle(event.target.value)
     }
 
     const amountChangeHandler = (event) => {
-        console.log(event.target.value)
+        setEnteredAmount(event.target.value)
     }
 
     const dateChangeHandler = (event) => {
-        console.log(event.target.value)
+        setEnteredDate(event.target.value)
     }
 
     return (
